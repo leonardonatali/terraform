2ef8165ff1ec8cf45bd5c98f44a0579d017a5da5
Leonardo Natali <leonardo.natali@b2wdigital.com>
2022-03-14 11:28:32 -0300

    feat: add output support

--- local_file.tf
+++ local_file.tf
@@ -3,7 +3,23 @@ resource "local_file" "example" {
   filename = "local_file.txt"
 }
 
+data "local_file" "example_datasource" {
+  filename = "local_file.txt"
+}
+
 variable "content" {
     type = string
-    default = "This is the default content"
+    default = "This is the default content."
+}
+
+output "file-id" {
+    value = local_file.example.id
+}
+
+output "file-permission" {
+    value = local_file.example.file_permission
+}
+
+output "datasource-result" {
+    value = data.local_file.example_datasource
 }
