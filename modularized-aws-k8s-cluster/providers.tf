844bd2b1e14004b41ecdd3c4ec97f04bb2e0abfc
Leonardo Natali <leonardo.natali@b2wdigital.com>
2022-03-15 16:32:33 -0300

    feat: add remote backend config

--- modularized-aws-k8s-cluster/providers.tf
+++ modularized-aws-k8s-cluster/providers.tf
@@ -1,12 +1,17 @@
 terraform {
   required_version = "1.1.7"
+  # backend "s3" {
+  #   bucket = "terraform"
+  #   key    = "terraform.tfstate"
+  #   region = "us-east-1"
+  # }
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.5.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.2.2"
     }
   }
